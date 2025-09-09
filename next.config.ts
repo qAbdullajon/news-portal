c7544d879350a284f66f6c082dc7a0d46c212953
qAbdullajon <azikmelor7705@gmail.com>
2025-09-09 06:18:26 +0500

    image upload fix

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "www.planetware.com",
-      "images.unsplash.com",
-      "thumbs.dreamstime.com",
-      "example.com",
-      "good-nature-blog-uploads.s3.amazonaws.com",
-      "res.cloudinary.com",
-    ],
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.planetware.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.example.com",
+        hostname: "**.cloudinary.com",
       },
     ],
   },
